a8a4fecbe5ec3b20e812049f735bd0f33e0998d8
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2022-03-27 13:35:18 -0600

    set grid rows

--- js/components/combatant.js
+++ js/components/combatant.js
@@ -2,24 +2,24 @@ export const combatant = Vue.component('combatant', {
   props: ['actor'],
   template: `
     <div class="grid-container">
-      <div class="grid-col-1"> Name </div> <div class="grid-col-2"> {{ actor.name }} </div>
-      <div class="grid-col-1"> HP </div> <div class="grid-col-2"> {{ actor.hp.value }} </div>
-      <div class="grid-col-1"> Weapon </div>  
-      <div class="grid-col-2">
+      <div class="grid-col-1 grid-row-1"> Name </div> <div class="grid-col-2 grid-row-1"> {{ actor.name }} </div>
+      <div class="grid-col-1 grid-row-2"> HP </div> <div class="grid-col-2 grid-row-2"> {{ actor.hp.value }} </div>
+      <div class="grid-col-1 grid-row-3"> Weapon </div>  
+      <div class="grid-col-2 grid-row-3">
         <i @click="$emit('swap-weapon', actor._id, false)" class="fa fa-caret-left caret"></i>
         <span>{{ actor.weapon.name }}</span>
         <i @click="$emit('swap-weapon', actor._id, true)" class="fa fa-caret-right caret"></i>
       </div>
-      <div class="grid-col-1">
+      <div class="grid-col-1 grid-row-4">
         <span class="underline">Armor</span>
       </div>
-      <div class="grid-col-2">
+      <div class="grid-col-2 grid-row-4">
         <span class="underline">AC Bonus</span>
       </div>
-      <div class="grid-col-1">
+      <div class="grid-col-1 grid-row-5">
         <div v-for="armor of actor.armors"> {{ armor.name }} </div>
       </div>
-      <div class="armor-bonuses">
+      <div class="grid-col-2 grid-row-5">
         <div v-for="armor of actor.armors"> {{ armor.base_ac.value }} </div>
       </div>
     </div>
